f6c5efabb70faa970f56c9a0ecf97daa6108a087
Aaronrblxbbq <124796861+aaronrblxbbq@users.noreply.github.com>
2025-03-12 14:40:02 +0200

    Removed code from defeatchanges.cmd
    Yep. I removed all the code from defeatchanges.cmd, to make sure that diffrent variables will be changed to do something dangerous.

--- defeatchanges.cmd
+++ defeatchanges.cmd
@@ -1,22 +1,5 @@
 @echo off
-
-REM Enable Task Manager
-REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /f
-
-echo Operation Completed Succesfully.
-timeout /t 2 /nobreak >nul
-cls
-REM Enable Command Prompt
-REG DELETE "HKCU\Software\Policies\Microsoft\Windows\System" /v "DisableCMD" /f
-
-cls
-echo Operation Completed Succesfully.
-timeout /t 2 /nobreak >nul
-
-REM Enable UAC
-REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "1" /f
-
-cls
-echo Operation Compled Successfully. 
-timeout /t 2 /nobreak >nul
+title Hello There!
+echo This is the file, what will cancel all changes. I did remove the code from here, becouse it can be used on dangerous use.
 pause
+exit
